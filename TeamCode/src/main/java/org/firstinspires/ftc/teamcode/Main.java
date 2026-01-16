e5281c77b9b42e5510fc7e64de9411be1c118c19
robotics <gavinatkinson08@gmail.com>
2026-01-16 15:31:01 -0800

    V1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Main")
 public class Main extends LinearOpMode {
@@ -20,14 +20,13 @@ public class Main extends LinearOpMode {
         DcMotor leftFront = hardwareMap.get(DcMotor.class, "front left");
         DcMotor rightRear = hardwareMap.get(DcMotor.class, "back right");
         DcMotor leftRear = hardwareMap.get(DcMotor.class, "back left");
-        DcMotor slider = hardwareMap.get(DcMotor.class, "linear slider");
-        Servo arm = hardwareMap.get(Servo.class, "arm");
-        CRServo claw = hardwareMap.get(CRServo.class, "claw");
+        DcMotorEx launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        CRServo servo1 = hardwareMap.get(CRServo.class, "battery side");
+        CRServo servo2 = hardwareMap.get(CRServo.class, "opposide");
 
         // Reverse the right side.
-        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-
         rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
         leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
         // This makes the robot BRAKE when power becomes zero. The other
@@ -36,13 +35,10 @@ public class Main extends LinearOpMode {
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        slider.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
 
         waitForStart();
         if (opModeIsActive()) {
             // Put run blocks here.
-            // Game pad 1
 
             while (opModeIsActive()) {
                 if (gamepad1.left_stick_y > 0.5 && gamepad1.left_stick_x > 0.5) {
@@ -85,16 +81,6 @@ public class Main extends LinearOpMode {
                     leftFront.setPower(-0.75);
                     rightRear.setPower(-0.75);
                     leftRear.setPower(0.75);
-                } else if (gamepad1.right_bumper) {
-                    rightFront.setPower(0.75);
-                    leftFront.setPower(-0.75);
-                    rightRear.setPower(0.75);
-                    leftRear.setPower(-0.75);
-                } else if (gamepad1.left_bumper) {
-                    rightFront.setPower(-0.75);
-                    leftFront.setPower(0.75);
-                    rightRear.setPower(-0.75);
-                    leftRear.setPower(0.75);
                 } else {
                     rightFront.setPower(0);
                     leftFront.setPower(0);
@@ -102,46 +88,48 @@ public class Main extends LinearOpMode {
                     leftRear.setPower(0);
                 }
                 if (gamepad1.right_stick_x > 0.5) {
-                    rightFront.setPower(1);
+                    rightFront.setPower(-1);
                     leftFront.setPower(-1);
                     rightRear.setPower(1);
-                    leftRear.setPower(-1);
+                    leftRear.setPower(1);
                 } else if (gamepad1.right_stick_x < -0.5) {
-                    rightFront.setPower(-1);
+                    rightFront.setPower(1);
                     leftFront.setPower(1);
                     rightRear.setPower(-1);
-                    leftRear.setPower(1);
+                    leftRear.setPower(-1);
                 }
 
-                // Game pad 2
-
-                if (gamepad2.dpad_up) {
-                    slider.setPower(-1);
-                } else if (gamepad2.dpad_down) {
-                    slider.setPower(1);
-                } else {
-                    slider.setPower(0);
-                }
                 if (gamepad2.right_bumper) {
-                    claw.setPower(1);
+                    servo2.setPower(-0.5);
+                    servo1.setPower(0.5);
                 } else if (gamepad2.left_bumper) {
-                    claw.setPower(-1);
+                    servo2.setPower(0.5);
+                    servo1.setPower(-0.5);
                 } else {
-                    claw.setPower(0);
-                }
-                if (gamepad2.y) {
-                    arm.setPosition(0.0);
-                } else if (gamepad2.b) {
-                    arm.setPosition(0.1);
-                } else if (gamepad2.a) {
-                    arm.setPosition(0.23);
+                    servo2.setPower(0);
+                    servo1.setPower(0);
                 }
 
-                telemetry.addData("Arm position: ", arm.getPosition());
-                telemetry.addData("slider position: ", slider.getCurrentPosition());
+                if (gamepad2.a) {
+                    launcher.setVelocity(1600);
+                    telemetry.addData("Current speed:", launcher.getVelocity());
+                } else {
+                    launcher.setVelocity(0);
+                }
+
                 telemetry.update();
+
+
+
 
             }
         }
     }
 }
+
+
+
+
+
+
+
